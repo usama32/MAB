b15fb1584095ed7eb7b316e7bbb3228819fae84f
Usama Mehmood <usama@omaemirates.com>
2024-02-20 13:20:26 +0500

    initial commit

---
+++ emvQRgenerator/src/main/java/com/mabapps/emvqrgenerator/EMVCOQR.java
@@ -0,0 +1,10 @@
+package com.mabapps.emvqrgenerator;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class EMVCOQR {
+    public static void getQR (Context c){
+        Toast.makeText(c,"testing msg!", Toast.LENGTH_SHORT).show();
+    }
+}
